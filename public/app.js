471dcf0af6b785ada9d04831c513684fa5e284a6
Tomasz <sigma345@wp.pl>
2022-11-26 23:12:12 +0100

    add .env file, dotenv in app.ts, connect to mongoo

--- public/app.js
+++ public/app.js
@@ -1,1 +1,26 @@
 "use strict";
+//import express from 'express';
+//import mongoose from 'mongoose';
+const dbURI = require('../MongooseKey');
+const express = require('express');
+//import {Server} from 'http';
+const app = express();
+app.listen(3002);
+app.get('/', (req, res) => {
+    //res.send('<p> Udało się </p>');
+    console.log('dupa');
+});
+//add mongoose
+/*
+mongoose.connect(dbURI)
+.then((result) => app.listen(3001))
+.catch((err) => console.log(err));
+
+app.get('/home', (res : Response, req : Request) => {
+//res.send('<p>To musi się udać </p>');
+console.log('to jest mój kod');
+});
+*/
+//add dbURI key
+//mongoose.connect()
+//conect mongoose to server
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,36 @@
-import express from 'express';
-import mongoose from 'mongoose';
+//import path from "path";
 
+const mongoose = require('mongoose');
+//const dbURI = require('../MongooseKey');
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
 
 
 const app = express();
+const source = process.env.DATABASE_URL;
+
+
+app.get('/', (req :Request  , res:Response) => {
+    //res.send('<p> Udało się </p>');
+    console.log('dupa');
+});
+
 //add mongoose
 
-//add dbURI key
+mongoose.connect(source,{
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+    })
+.then(() => app.listen(3001))
+.catch((err:string) => console.log(err));
 
+app.get('/home', (res : Response, req : Request) => {
+console.log("jesteśmy w domu");
+});
+
+
+
+//add dbURI key
+//mongoose.connect()
 //conect mongoose to server
