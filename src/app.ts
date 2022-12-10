55b242a3df2207262cdf4424a3dcf557e497ae71
Tomasz <sigma345@wp.pl>
2022-12-10 22:31:09 +0100

    remove unnecessary code

--- src/app.ts
+++ src/app.ts
@@ -17,22 +17,13 @@ app.use(express.urlencoded({extended: true}));
 
 let source:string = process.env.DATABASE_URL!;
 
-//text 
-app.get('/', (req :Request  , res:Response) => {
-    //res.send('<p> Udało się </p>');
-    console.log('dupa');
-});
-
 
 //add mongoose
 mongoose.connect(source)
 .then(() => app.listen(3001))
 .catch((err:string) => console.log(err));
 
-//text mongoose
-app.get('/home', (res : Response, req : Request) => {
-console.log("jesteśmy w domu");
-});
+
 
 app.get('/all', (req:Request, res:Response) => {
     employeeShema.find()
